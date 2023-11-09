6c21081ed38a0efd9bed401861ca711e07633636
HMarinNolasco <hernan.marin.nolasco@gmail.com>
2023-11-08 19:28:56 -0500

    Done Homework M05

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -3,34 +3,47 @@
 function devolverPrimerElemento(array) {
    // Retornar el primer elemento del arreglo recibido por parámetro.
    // Tu código:
+   return array[0];
 }
 
 function devolverUltimoElemento(array) {
    // Retornar el último elemento del arreglo recibido por parámetro.
    // Tu código:
+   return array.at(-1);
 }
 
 function obtenerLargoDelArray(array) {
    // Retornar la longitud del arreglo recibido por parámetro.
    // Tu código:
+   return array.length;
 }
 
 function incrementarPorUno(array) {
    // El arreglo recibido por parámetro contiene números.
    // Retornar un arreglo con los elementos incrementados en +1.
    // Tu código:
+   var arrayIncrementado = [];
+   array.forEach(function(elemento) {
+      elemento = elemento + 1;
+      arrayIncrementado.push(elemento);
+   });
+   return arrayIncrementado;
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
    // Agrega el "elemento" al final del arreglo recibido.
    // Retorna el arreglo.
    // Tu código:
+   array.push(elemento)
+   return array
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
    // Agrega el "elemento" al comienzo del arreglo recibido.
    // Retorna el arreglo.
    // Tu código:
+   array.unshift(elemento)
+   return array;
 }
 
 function dePalabrasAFrase(palabras) {
@@ -39,43 +52,75 @@ function dePalabrasAFrase(palabras) {
    // con un espacio entre cada palabra.
    // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'.
    // Tu código:
+   return palabras.join(" ");
 }
+
+console.log(dePalabrasAFrase(['Hola', 'Mundo', 'Soy', 'Hernan!']))
 
 function arrayContiene(array, elemento) {
    // Verifica si el elemento existe dentro del arreglo recibido.
    // Retornar true si está, o false si no está.
    // Tu código:
+   return array.includes(elemento)
 }
 
 function agregarNumeros(arrayOfNums) {
    // El parámetro "arrayOfNums" debe ser un arreglo de números.
    // Suma todos los elementos y retorna el resultado.
    // Tu código:
+   let suma = arrayOfNums.reduce(function(acum, elemento){
+      return acum + elemento
+   }, 0)
+
+   return suma;
 }
+
+console.log(agregarNumeros([1,2,66]));
 
 function promedioResultadosTest(resultadosTest) {
    // El parámetro "resultadosTest" es un arreglo de números.
    // Itera (en un bucle) los elementos del arreglo y devuelve el promedio de las notas.
    // Tu código:
+   return agregarNumeros(resultadosTest) / obtenerLargoDelArray(resultadosTest)
 }
 
 function numeroMasGrande(arrayOfNums) {
    // El parámetro "arrayOfNums" es un arreglo de números.
    // Retornar el número más grande.
    // Tu código:
+   var masGrande = arrayOfNums[0];
+   arrayOfNums.forEach( el=> {
+      if (el > masGrande) masGrande = el;
+   })
+
+   return masGrande;
 }
+
+console.log(numeroMasGrande([1,2,88,4,22,222,121,-2222,9999,Infinity]));
 
 function multiplicarArgumentos() {
    // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto.
    // Si no se pasan argumentos retorna 0. Si se pasa un argumento, simplemente retórnalo.
    // [PISTA]: "arguments" es un arreglo.
    // Tu código:
+   var total= 1;
+   for (var el of arguments) {
+      total = total * el
+   }
+   return total
 }
+
+console.log(multiplicarArgumentos(4,1,3));
 
 function cuentoElementos(array) {
    // Desarrolla una función que retorne la cantidad de elementos del arreglo cuyo valor sea mayor que 18.
    // Tu código:
+   const mayores = array.filter(el => el > 18)
+   return mayores.length;
 }
+
+console.log(cuentoElementos([1,22,222,11,18,22]));
+
 
 function diaDeLaSemana(numeroDeDia) {
    // Supongamos que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente.
@@ -84,36 +129,68 @@ function diaDeLaSemana(numeroDeDia) {
    // Tu código:
 }
 
+
+
+
 function empiezaConNueve(num) {
    // Esta función recibe por parámetro un número.
    // Debe retornar true si el entero inicia con 9 y false en otro caso.
    // Tu código:
+   return num.toString()[0] === "9"
 }
+
+console.log(empiezaConNueve(90));
+console.log(empiezaConNueve(19))
+
 
 function todosIguales(array) {
    // Si todos los elementos del arreglo son iguales, retornar true.
    // Caso contrario retornar false.
    // Tu código:
+   return array.every(el => el === array[0]);
 }
 
-function mesesDelAño(array) {
+console.log(todosIguales([1,1,1,2]))
+
+
+function mesesDelAno(array) {
    // El arreglo contiene algunos meses del año desordenados. Debes recorrerlo, buscar los meses "Enero",
    // "Marzo" y "Noviembre", guardarlos en un nuevo arreglo y retornarlo.
    // Si alguno de los meses no está, retornar el string: "No se encontraron los meses pedidos".
    // Tu código:
+   const mesesEncontrados = array.filter(mes => (mes === "Enero" || mes === "Marzo" || mes === "Noviembre"))
+   if (mesesEncontrados.length === 3) {
+      return mesesEncontrados; // Retorna el arreglo con los meses encontrados.
+   } else {
+      return "No se encontraron los meses pedidos"; // Retorna el mensaje de error.
+   }
 }
+console.log(mesesDelAno(['Enero','Marzo','Noviembre']))
+
 
 function tablaDelSeis() {
    // Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
    // La función devuelve un arreglo con los resultados de la tabla de multiplicar del 6 en orden creciente.
    // Tu código:
+   var tabla = [];
+   for(i=0; i<=10; i++){
+      tabla.push(i*6);
+   }
+   return tabla;
 }
+
+console.log(tablaDelSeis())
+
 
 function mayorACien(array) {
    // La función recibe un arreglo con enteros entre 0 y 200.
    // Recorrerlo y retornar un arreglo con todos los valores mayores a 100 (no incluye el 100).
    // Tu código:
+   return array.filter(el => el > 100 && el < 201)
+
 }
+
+console.log(mayorACien([1,2,100,110,220,188,199,200]));
 
 /* ----------------------------------------------------------------------------------
 💪 EXTRA CREDIT EXTRA CREDIT EXTRA CREDIT EXTRA CREDIT EXTRA CREDIT  EXTRA CREDIT 💪
